beeca58015032aa17dc52a7b6405e56fb72cd037
barryhughes <barry@codingkills.me>
2019-03-20 11:46:19 -0500

    Wrap settnigs fields in <table> tags

--- src/views/admin/options.php
+++ src/views/admin/options.php
@@ -2,10 +2,12 @@
 	<h1> <?php echo esc_html_x( 'Tribe Fools', 'settings page heading', 'tribe-fools' ); ?> </h1>
 
 	<form method="post" action="options.php">
-		<?php
-			settings_fields( 'tribe-fools' );
-			do_settings_fields( 'tribe-fools-settings', 'general-settings' );
-			submit_button();
-		?>
+		<?php settings_fields( 'tribe-fools' ); ?>
+
+		<table id="tribe_fools_general_settings">
+			<?php do_settings_fields( 'tribe-fools-settings', 'general-settings' ); ?>
+		</table>
+
+		<?php submit_button(); ?>
 	</form>
 </div>
