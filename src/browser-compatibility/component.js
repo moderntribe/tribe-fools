16131030373676196e5da33e335784bebfd54fed
Neal Fennimore <hi@neal.codes>
2019-03-20 15:21:20 -0400

    Fix not confirming on fe

--- src/browser-compatibility/component.js
+++ src/browser-compatibility/component.js
@@ -1,17 +1,20 @@
-const { Component } = React;
+const { Component, Fragment } = React;
+
+const script = `
+	window.confirm( 'You need to upgrade your browser.' ) && (
+		window.location.href = 'https://www.amazon.com/Netscape-Navigator-Personal-Ver-Windows/dp/B0028M1T9E/'
+	)
+`;
 
 export default class Prompt extends Component {
 	static propTypes = {}
 
-	componentDidMount() {
-		if ( window.confirm( 'You need to upgrade your browser.' ) ) {
-			window.location.href = 'https://www.amazon.com/Netscape-Navigator-Personal-Ver-Windows/dp/B0028M1T9E/';
-		}
-	}
-
 	render() {
 		return (
-			<img src="https://uc47d3537856c015f89e52ffe5cf.previews.dropboxusercontent.com/p/thumb/AAU41k3lWq6k8bfuTx8ro_xbcAhhjO_2GaNZINHEGsHTFFDQgetHQnIk6lJB1qr3xxjFenll73RuE2zgxTPpsodegGLeizHNejx8p0VuVPBdZG6cTDQCBm9ckyWEgOJfk3C5THi3IKobgBSTjrInt-PpyhNQ-2uczYBskc_tOxycXThUvSWJQKUMy2YEq0iuu1dmcC_kGg4tZMCxOPX4b7jWgCuOkZxykruWSo_ElLqD39EKveF9rq2lv6ixxYgdBNl78CpnLABFghC_cEYTmnJcuYBSRo5j93fDYByweCgx9A0mPTd0wco4Vv2PRffqXc7xve1F0JUcmoaWdVHwabR4/p.png?size_mode=5" alt="" />
+			<Fragment>
+				<script dangerouslySetInnerHTML={ { __html: script } }></script>
+				<img src="https://uc47d3537856c015f89e52ffe5cf.previews.dropboxusercontent.com/p/thumb/AAU41k3lWq6k8bfuTx8ro_xbcAhhjO_2GaNZINHEGsHTFFDQgetHQnIk6lJB1qr3xxjFenll73RuE2zgxTPpsodegGLeizHNejx8p0VuVPBdZG6cTDQCBm9ckyWEgOJfk3C5THi3IKobgBSTjrInt-PpyhNQ-2uczYBskc_tOxycXThUvSWJQKUMy2YEq0iuu1dmcC_kGg4tZMCxOPX4b7jWgCuOkZxykruWSo_ElLqD39EKveF9rq2lv6ixxYgdBNl78CpnLABFghC_cEYTmnJcuYBSRo5j93fDYByweCgx9A0mPTd0wco4Vv2PRffqXc7xve1F0JUcmoaWdVHwabR4/p.png?size_mode=5" alt="" />
+			</Fragment>
 		);
 	}
 }
